7607a07604c1c11086267430efe0e010b710d3b8
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-05-29 16:20:23 +0200

    feat: add more holyc features

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import("./add.c").then(({add}) => {
+const {init} = require("holycjs");
+
+import("./add.c").then(({add, test, memory}) => {
+  init(memory);
+
   document.body.innerHTML = "1+1=" + add(1, 1);
+
+  test();
 });
